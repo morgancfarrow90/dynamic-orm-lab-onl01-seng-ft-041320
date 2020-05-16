023011415fcc3a68b322ea4940057013ab16edc1
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-16 22:31:40 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,8 +48,8 @@ class InteractiveRecord
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
-def self.find_by(attribute)
+def self.find_by_name(attribute)
   sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-  DB[:conn].execute(sql, attribute)
+  DB[:conn].execute(sql, name)
 end
 end
