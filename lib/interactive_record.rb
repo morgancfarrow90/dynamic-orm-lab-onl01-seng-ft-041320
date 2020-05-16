f0f710839fe40f1414323b412ba7b4e304f3f806
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-16 22:36:08 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,4 +52,10 @@ def self.find_by_name(name)
   sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
   DB[:conn].execute(sql, name)
 end
+
+def self.find_by(x)
+  sql = "SELECT * FROM #{self.table_name}"
+  DB[:conn].execute(sql, x).map do |row|
+    if 
+
 end
